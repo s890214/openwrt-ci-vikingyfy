7fb8f7ff98d2cdc6722bef66d0c4b0e3c771cf90
s890214 <s890214@users.noreply.github.com>
2026-06-29 00:32:35 +0800

    fix: 删 sing-box 更新(不需要), 恢复 Settings.sh 原有功能
    - Packages.sh: 删 UPDATE_VERSION sing-box 更新，OpenClash 自带核心管理
    - Settings.sh: 恢复 VIKINGYFY 原有的 WiFi/密码/背景图修改功能
    - Handles.sh: 保留 NSS ECM 修复（来自原项目 Roc-script.sh）

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
-# 固件个性化设置
+# 基于 VIKINGYFY/OpenWRT-CI 的 Settings.sh
 
 WRT_PATH="$GITHUB_WORKSPACE/wrt"
+
+# 修改默认IP
+# sed -i 's/192.168.1.1/192.168.2.1/g' $WRT_PATH/package/base-files/files/bin/config_generate
+
+# 修改主机名
+# sed -i "s/hostname='.*'/hostname='Roc'/g" $WRT_PATH/package/base-files/files/bin/config_generate
 
 # 修改固件版本签名
 sed -i "s#_('Firmware Version'), (L\\.isObject(boardinfo\\.release) ? boardinfo\\.release\\.description + ' / ' : '') + (luciversion || ''),# \\
 _('Firmware Version'), \\
 (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + \\
 'Built by SONG88 $(date \"+%Y-%m-%d\")',#" $WRT_PATH/feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+
+# 修改默认主题
+sed -i "s/mediaurlimg\/iphone\/background.jpg/mediaurlimg\/iphone\/dark.jpg/g" $(find $WRT_PATH/feeds/luci/ -maxdepth 5 -type f -wholename "*/luci-theme-argon/htdocs/luci-static/resources/view/system/system.js")
+
+# 修改默认 WiFi 设置
+if [ -f $WRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh ]; then
+	sed -i "/wifi-iface\[0\]/,/}/ s/option ssid.*/option ssid '$WRT_SSID'/g; /wifi-iface\[0\]/,/}/ s/option encryption.*/option encryption 'psk2'/g; /wifi-iface\[0\]/,/}/ s/option key.*/option key '$WRT_WORD'/g" $WRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+fi
+
+# 修改默认密码
+if [ -f $WRT_PATH/package/base-files/files/etc/shadow ]; then
+	sed -i "s/root:.*/root::0:0:99999:7:::/g" $WRT_PATH/package/base-files/files/etc/shadow
+fi
 
 # 移除要替换的包（避免和 Packages.sh 里 clone 的版本冲突）
 rm -rf $WRT_PATH/feeds/luci/applications/luci-app-argon-config
@@ -17,6 +36,6 @@ rm -rf $WRT_PATH/feeds/packages/net/open-app-filter
 rm -rf $WRT_PATH/feeds/packages/net/frp
 rm -rf $WRT_PATH/feeds/packages/lang/golang
 
-# 清理不需要的 feeds 包（减小编译体积）
+# 清理冗余文件
 rm -rf $WRT_PATH/feeds/luci/applications/luci-app-ssr-plus
 rm -rf $WRT_PATH/feeds/packages/net/{adguardhome,dns2tcp,dnstap,dpdk,haproxy,mwan3,ndppd,netcat,nginx,pcap-dnsproxy,pdnsd,redsocks,stubby}
